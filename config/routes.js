f7df67852b9971d354015006fe425899e255eb7d
IlanL75 <anatoly.leytman@gmail.com>
2018-10-26 10:35:14 -0700

    MVP

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig');
+const jwtKey = require('../_secrets/keys').jwtKey;
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +14,51 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  db('users')
+    .insert(credentials)
+    .then(ids => {
+      const id = ids[0];
+      const token = generateToken({ username: credentials.username });
+      res.status(201).json({ newUserId: id, token });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+}
+
+function generateToken(user) {
+  const jwtPayload = {
+    ...user,
+  };
+  const jwtOptions = {
+    expiresIn: '6h',
+  };
+  return jwt.sign(jwtPayload, jwtKey, jwtOptions);
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ welcome: user.username, token });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ err });
+    });
 }
 
 function getJokes(req, res) {
